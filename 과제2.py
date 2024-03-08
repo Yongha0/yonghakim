78de70b9e9128ffef8427b8245471d8db4ef1bae
yongha <dydgk0621@naver.com>
2024-03-08 15:19:59 +0900

    second commit

---
+++ 과제2.py
@@ -0,0 +1,48 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print ('Hello World')
+Hello World
+>>> print ('Hello World')
+Hello World
+>>> print ('Hello World'), print ('Hello World'), print ('Hello World')
+Hello World
+Hello World
+Hello World
+(None, None, None)
+>>> print ('Hello', 'World')
+Hello World
+>>> print ('Hello'+ 'World')
+HelloWorld
+>>> print(100)
+100
+>>> print(2+4)
+6
+>>> 100
+100
+>>> 10 + 20
+30
+>>> 10-20
+-10
+>>> 10*20
+200
+>>> 10/20
+0.5
+>>> 123*8765
+1078095
+>>> 10+20*30
+610
+>>> x=100
+>>> y=200
+>>> x + y
+300
+>>> x-y
+-100
+>>> x*y
+20000
+>>> x/y
+0.5
+>>> x**2
+10000
+>>> n = 5
+>>> n**2
+25
